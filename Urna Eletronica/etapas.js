0b737d7df3b4da486dd60a530f63e19aed23e428
Rickkdinho <jose.rick2016@gmail.com>
2021-02-19 22:08:56 -0300

    comitando arquivos

---
+++ Urna Eletronica/etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Jean Grey',
+                partido: 'Os X-men',
+                fotos: [
+                    {url:'38111.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Pietro (Mercúrio)',
+                partido: 'Liga do Magneto',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '51222',
+                nome: 'Vampira',
+                partido: 'Os X-men',
+                fotos: [
+                    {url:'51222.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '33377',
+                nome: 'Gambit',
+                partido: 'Liga do Magneto',
+                fotos: [
+                    {url:'33377.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '55',
+                nome: 'Charles Xavier',
+                partido: 'Os X-men',
+                vice: 'Ororo Munroe',
+                fotos: [
+                    {url:'55.png', legenda: 'Prefeito'},
+                    {url:'55_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Magneto',
+                partido: 'Liga do Magneto',
+                vice: 'Mística',
+                fotos: [
+                    {url:'84.png', legenda: 'Prefeito'},
+                    {url:'84_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
